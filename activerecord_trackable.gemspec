398e9d4b2c89675f022cf3d63973274827c9041f
Erez Rabih <erez@sygen.co>
2012-07-14 01:22:57 +0300

    rspec test suit setup

--- activerecord_trackable.gemspec
+++ activerecord_trackable.gemspec
@@ -18,4 +18,7 @@ Gem::Specification.new do |s|
   s.add_dependency "rails", "~> 3.2.6"
 
   s.add_development_dependency "sqlite3"
+  s.add_development_dependency "rspec-rails"
+  s.add_development_dependency "guard-rspec"
+  s.add_development_dependency "guard-spork"
 end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,21 @@
+require 'rubygems'
+require 'spork'
+
+Spork.prefork do
+  ENV["RAILS_ENV"] ||= 'test'
+  require File.expand_path("../dummy/config/environment", __FILE__)
+  require 'rspec/rails'
+  require 'rspec/autorun'
+
+  Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+
+  RSpec.configure do |config|
+    config.fixture_path = "#{::Rails.root}/spec/fixtures"
+    config.use_transactional_fixtures = true
+    config.infer_base_class_for_anonymous_controllers = false
+  end
+end
+
+Spork.each_run do
+  # This code will be run each time you run your specs.
+end
