56ec0de9976d650d8a02df073d5ce3d42b2d6522
Erez Rabih <erez@sygen.co>
2012-07-14 03:07:58 +0300

    generator progress

---
+++ spec/dummy/lib/generators/attributes_tracker/attributes_tracker_generator.rb
@@ -0,0 +1,9 @@
+class AttributesTrackerGenerator < Rails::Generators::NamedBase
+  include ActiveSupport::Inflector
+  source_root File.expand_path('../templates', __FILE__)
+  argument :attrs, :type => :array, :default => []
+
+  def generate_track_attributes_call_on_model_file
+    inject_into_class "app/models/#{name.underscore}.rb", name.camelize.constantize , "track_attributes"
+  end
+end
